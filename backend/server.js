c535b685f798185722a84af86eec65469ba0a531
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-03-18 20:44:35 -0300

    correção do nome das variaveis de ambiente

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ app.use(cors({
 /* ================= CONFIG ================= */
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const replicate = new Replicate({
